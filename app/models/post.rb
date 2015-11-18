8f3111786b93b7c737fe5a2b127584f5dd30db75
jonathanlepse <jonathan.lepselter@gmail.com>
2015-11-18 16:28:33 +0000

    creates comments model and updates associtaions in post/user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  belongs_to :user # 1 to many association this is the M side it is singular b/c each post belongs_to 1 user
+  belongs_to :creator, foreign_key: "user_id", class_name: "User" # 1 to many association this is the M side it is singular b/c each post belongs_to 1 user, but if we change the name here to creator, rails now expects a belongs_to to have a creator object and a creator_id foreign key, but their isnt one so we have to spcifiy the foreign key and the class or model it came from
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :posts  # this is 1 to many association this is the 1 side, it is plural because a user has many posts
+  has_many :comments
 end
